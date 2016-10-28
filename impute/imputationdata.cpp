91003eb1d9928bf60d2eaefabe5421bb47dea95e
James Grover <grover@goldenhelix.com>
2016-10-28 10:56:13 -0600

    Case 10145: Ported RefHapSegs and polished the other recent ports.

--- impute/imputationdata.cpp
+++ impute/imputationdata.cpp
@@ -1,23 +1,21 @@
 #include "impute/imputationdata.h"
 
-
 class HapSegData
 {
 public:
 
-  HapSegData(SampleHapPairs* refHapPairs, int start, int end);
+  HapSegData(const SampleHapPairs &refHapPairs, int start, int end);
 
-  QVector<int> hap2Seq() { return _hap2seq; }
+  QVector<int> hap2Seq() const { return _hap2seq; }
 
-  QVector<int> seq2Hap();
+  QVector<int> seq2Hap() const;
 
 private:
-  void highMafUpdate(int marker);
-  int indexOfAllele(QList<int> list, int allele);
-  void mapAlleleToNewSeq(QList<int> list, int allele);
+  void highMafUpdate(const SampleHapPairs &refHapPairs, int marker);
+  int indexOfAllele(const QList<int> &list, int allele) const;
+  void mapAlleleToNewSeq(QList<int> &list, int allele);
   void updateHap2Seq(int h, int seq);
 
-  SampleHapPairs* _refHapPairs;
   QVector<int> _hap2seq;
   QList<int> _seq2Cnt;
 
@@ -25,18 +23,17 @@ private:
   QList< QList<int> > _seq2AlleleMap;
 };
 
-HapSegData::HapSegData(SampleHapPairs* refHapPairs, int start, int end)
+HapSegData::HapSegData(const SampleHapPairs &refHapPairs, int start, int end)
 {
-  _refHapPairs = refHapPairs;
-  _hap2seq.fill(0, refHapPairs->nHaps());
+  _hap2seq.fill(0, refHapPairs.nHaps());
   _seq2Cnt.append(_hap2seq.length());
   _seq2AlleleMap.append(_emptyList);
 
   for (int m=start; m<end; ++m)
-    highMafUpdate(m);
+    highMafUpdate(refHapPairs, m);
 }
 
-QVector<int> HapSegData::seq2Hap()
+QVector<int> HapSegData::seq2Hap() const
 {
   QVector<int> seqToHap(_seq2AlleleMap.size(), -1);
 
@@ -51,7 +48,7 @@ QVector<int> HapSegData::seq2Hap()
   return seqToHap;
 }
 
-void HapSegData::highMafUpdate(int marker)
+void HapSegData::highMafUpdate(const SampleHapPairs &refHapPairs, int marker)
 {
   for (int j=0, n = _seq2AlleleMap.size(); j<n; ++j)
     _seq2AlleleMap[j].clear();
@@ -59,8 +56,8 @@ void HapSegData::highMafUpdate(int marker)
   for (int h=0; h < _hap2seq.length(); ++h)
   {
     int seq = _hap2seq[h];
-    int allele = _refHapPairs->allele(marker, h);
-    QList<int> list = _seq2AlleleMap[seq];
+    int allele = refHapPairs.allele(marker, h);
+    QList<int> &list = _seq2AlleleMap[seq];
 
     if (list.isEmpty())
     {
@@ -79,7 +76,7 @@ void HapSegData::highMafUpdate(int marker)
   }
 }
 
-int HapSegData::indexOfAllele(QList<int> list, int allele)
+int HapSegData::indexOfAllele(const QList<int> &list, int allele) const
 {
   int index=0;
 
@@ -89,7 +86,7 @@ int HapSegData::indexOfAllele(QList<int> list, int allele)
   return index;
 }
 
-void HapSegData::mapAlleleToNewSeq(QList<int> list, int allele)
+void HapSegData::mapAlleleToNewSeq(QList<int> &list, int allele)
 {
   list.append(allele);
   list.append(_seq2AlleleMap.size());
@@ -104,9 +101,9 @@ void HapSegData::updateHap2Seq(int h, int seq)
   _seq2Cnt[_hap2seq[h]]++;
 }
 
-RefHapSeg::RefHapSeg(SampleHapPairs* refHapPairs, int start, int end)
+RefHapSeg::RefHapSeg(const SampleHapPairs &refHapPairs, int start, int end)
 {
-  Q_ASSERT_X(start >= 0  &&  start < end  &&  end <= refHapPairs->nMarkers(),
+  Q_ASSERT_X(start >= 0  &&  start < end  &&  end <= refHapPairs.nMarkers(),
              "RefHapSeg::RefHapSeg",
              "invalid start and/or end values");
 
@@ -118,12 +115,41 @@ RefHapSeg::RefHapSeg(SampleHapPairs* refHapPairs, int start, int end)
   _seqToHap = hapSegData.seq2Hap();
 }
 
-int RefHapSeg::allele(int marker, int seq)
+int RefHapSeg::allele(int marker, int seq) const
 {
   int refIndex = _start + marker;
   Q_ASSERT_X(marker >= 0  &&  refIndex < _end,
              "RefHapSeg::allele",
-             "marker < 0 || refIndex >= end");
+             "marker < 0 || refIndex >= _end");
 
-  return _refHapPairs->allele(refIndex, _seqToHap[seq]);
+  return _refHapPairs.allele(refIndex, _seqToHap[seq]);
+}
+
+RefHapSegs::RefHapSegs(const SampleHapPairs &refHapPairs, const QVector<int> &segStart, const QVector<int> &segEnd)
+{
+  int nMarkers = refHapPairs.nMarkers();
+  checkClusters(segStart, segEnd, nMarkers);
+  _segStart = segStart;
+  _segEnd = segEnd;
+  _refHapPairs = refHapPairs;
+
+  for(int j=0, n=segStart.length(); j<n; j++)
+  {
+    RefHapSeg rhs(refHapPairs, segStart[j], segEnd[j]);
+    _refHapSegs.append(rhs);
+  }
+}
+
+void RefHapSegs::checkClusters(const QVector<int> &starts, const QVector<int> &ends, int nMarkers)
+{
+  Q_ASSERT_X(starts.length() == ends.length(),
+             "RefHapSegs::checkClusters",
+             "starts.length() != ends.length()");
+
+  for (int j=0; j < starts.length(); ++j)
+  {
+    Q_ASSERT_X(starts[j] >= 0  &&  starts[j] < ends[j]  &&  ends[j] <= nMarkers,
+               "RefHapSegs::checkClusters",
+               "starts[j] < 0 || starts[j] >= ends[j] || ends[j] > nMarkers");
+  }
 }
--- impute/imputationdata.h
+++ impute/imputationdata.h
@@ -34,7 +34,7 @@ public:
    * @param chrom the chromosome index
    * @param geneticPosition the genetic position on the chromosome
    */
-  int basePos(int chrom, double geneticPosition){ return (int) (0.5 + geneticPosition / _scaleFactor); }
+  int basePos(int chrom, double geneticPosition) const { return (int) (0.5 + geneticPosition / _scaleFactor); }
 
   /**
    * Returns the genetic map position of the specified marker. The
@@ -42,7 +42,7 @@ public:
    *
    * @param marker a genetic marker
    */
-  double genPos(const Marker &marker) { return _scaleFactor * marker.pos(); }
+  double genPos(const Marker &marker) const { return _scaleFactor * marker.pos(); }
 
   /**
    * Returns the genetic map position of the specified genome coordinate.
@@ -51,14 +51,14 @@ public:
    * @param chrom the chromosome index
    * @param basePosition the base coordinate on the chromosome
    */
-  double genPos(int chrom, int basePosition) { return _scaleFactor * basePosition; }
+  double genPos(int chrom, int basePosition) const { return _scaleFactor * basePosition; }
 
   /**
    * Returns the scale factor that is multiplied by the chromosome position
    * to obtain the corresponding genetic map position
    * @return the scale factor.
    */
-  double scaleFactor() { return _scaleFactor; }
+  double scaleFactor() const { return _scaleFactor; }
 
 private:
   double _scaleFactor;
@@ -80,46 +80,139 @@ public:
    * @param start the starting marker index (inclusive)
    * @param end the ending marker index (exclusive)
    */
-  RefHapSeg(SampleHapPairs* refHapPairs, int start, int end);
+  RefHapSeg(const SampleHapPairs &refHapPairs, int start, int end);
 
   /**
    * Returns the reference haplotype pairs.
    */
-  SampleHapPairs* refHapPairs() const { return _refHapPairs; }
+  const SampleHapPairs& refHapPairs() const { return _refHapPairs; }
 
   /**
    * Return the number of reference allele sequences in this segment.
    */
-  int nSeq() { return _seqToHap.length(); }
+  int nSeq() const { return _seqToHap.length(); }
 
   /**
    * Return the index of the reference allele sequence in this segment
    * for the specified reference haplotype.
    * @param hap a haplotype index
    */
-  int seq(int hap) { return _hapToSeq[hap]; }
+  int seq(int hap) const { return _hapToSeq[hap]; }
 
   /**
    * Return the specified reference haplotype allele.
    * @param marker index of a marker in this segment
    * @param seq index of a reference allele sequence in this segment
    */
-  int allele(int marker, int seq);
+  int allele(int marker, int seq) const;
 
   /**
    * Returns the starting marker index (inclusive) of this segment.
    */
-  int start() { return _start; }
+  int start() const { return _start; }
 
   /**
    * Returns the ending marker index (exclusive) of this segment.
    */
-  int end() { return _end; }
+  int end() const { return _end; }
 
 private:
-  SampleHapPairs* _refHapPairs;
+  SampleHapPairs _refHapPairs;
   int _start;     // inclusive
   int _end;       // exclusive
   QVector<int> _hapToSeq;
   QVector<int> _seqToHap;
 };
+
+
+/**
+ * Class {@code RefHapSegs} represents reference haplotypes that span
+ * segments determined by non-overlapping clusters of markers.
+ *
+ * "Instances of class {@code RefHapSegs} are immutable."
+ */
+class RefHapSegs
+{
+public:
+
+  /**
+   * Constructs a new {@code RefHapSegs} instance from the specified data.
+   * @param refHapPairs the reference haplotype pairs
+   * @param segStart an array whose {@code j}-th element is the
+   * starting reference marker index (inclusive) for the {@code j}-th
+   * segment of markers
+   * @param segEnd an array whose {@code j}-th element is the
+   * ending reference marker index (exclusive) for the {@code j}-th
+   * segment of markers
+   */
+  RefHapSegs(const SampleHapPairs &refHapPairs, const QVector<int> &segStart, const QVector<int> &segEnd);
+
+  /**
+   * Returns the reference haplotype pairs.
+   */
+  const SampleHapPairs& refHapPairs() const { return _refHapPairs; }
+
+  /**
+   * Return the number of distinct reference allele sequences in the
+   * specified chromosome segment.
+   * @param segment index of a chromosome segment determined by
+   * the marker clusters
+   */
+  int nSeq(int segment) const { return _refHapSegs[segment].nSeq(); }
+
+  /**
+   * Return the number of markers in the specified chromosome segment.
+   * @param segment index of a chromosome segment determined by
+   * the marker clusters
+   */
+  int nMarkers(int segment) const { return _refHapSegs[segment].end() - _refHapSegs[segment].start(); }
+
+  /**
+   * Return the index of the allele sequence in the specified chromosome
+   * segment for the specified reference haplotype.
+   *
+   * @param segment index of a chromosome segment determined by
+   * the marker clusters
+   * @param hap a haplotype index
+   */
+  int seq(int segment, int hap) const { return _refHapSegs[segment].seq(hap); }
+
+  /**
+   * Return the specified reference haplotype allele.
+   *
+   * @param segment index of a chromosome segment determined by
+   * the marker clusters
+   * @param marker index of a marker in the specified interval
+   * @param seq index of a reference allele sequence in the specified
+   * interval
+   */
+  int allele(int segment, int marker, int seq) const { return _refHapSegs[segment].allele(marker, seq); }
+
+  /**
+   * Returns the number of marker segments.
+   */
+  int nSegs() const { return _segStart.length(); }
+
+  /**
+   * Returns the index of the first marker (inclusive) in the specified
+   * marker segment.
+   * @param segment an index of a marker segment
+   */
+  int segStart(int segment) const { return _segStart[segment]; }
+
+  /**
+   * Returns the index of the last marker (exclusive) in the specified
+   * marker segment.
+   * @param segment an index of a marker segment
+   */
+  int segEnd(int segment) const { return _segEnd[segment]; }
+
+private:
+  void checkClusters(const QVector<int> &starts, const QVector<int> &ends, int nMarkers);
+
+  QVector<int> _segStart;
+  QVector<int> _segEnd;
+  SampleHapPairs _refHapPairs;
+  QList<RefHapSeg> _refHapSegs;
+};
+
